02e9054b5198d42b96509fde5f19a38b5d6af7a3
Patrick Her <renfu.her@gmail.com>
2023-06-03 15:21:26 +0800

    update all

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
          * Package Service Providers...
          */
 
+         Intervention\Image\ImageServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -182,7 +184,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ])->toArray(),
 
 ];
