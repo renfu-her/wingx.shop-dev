b313d5fac4bc939bc8db88c32b6430c394323644
Patrick Her <renfu.her@gmail.com>
2023-06-03 22:11:15 +0800

    update model

--- app/Http/Controllers/Api/ImportDataController.php
+++ app/Http/Controllers/Api/ImportDataController.php
@@ -20,8 +20,36 @@ class ImportDataController extends Controller
 
         $data = json_decode($json, true);
 
+        $items = $data['items'];
 
-        dd($data);
+        foreach($items as $item){
+            $item_basic = $item['item_basic'];
+
+            $product = new Product();
+            $product->item_id = $item_basic['itemid'];
+            $product->name = $item_basic['name'];
+            $product->price = $item_basic['price'];
+            $product->price_max = $item_basic['price_max'];
+            $product->price_min = $item_basic['price_min'];
+            $product->image = $item_basic['image'];
+            $product->status = $item_basic['status'];
+
+            $product->save();
+
+            $product_id = $product->id;
+
+            $idx = 1;
+            foreach($item_basic['images'] as $image){
+                $product_image = new ProductImage();
+                $product_image->product_id = $product_id;
+                $product_image->image = $image;
+                $product_image->sort = $idx;
+                $product_image->save();
+
+                $idx++;
+            }
+
+        }
 
     }
 }
