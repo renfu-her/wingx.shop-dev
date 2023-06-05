0e0b2e5e9dea6f295b0692c349d189fac077f697
Patrick Her <renfu.her@gmail.com>
2023-06-05 09:41:01 +0800

    fix

--- lang/zh_TW/pagination.php
+++ lang/zh_TW/pagination.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 return [
     'next'     => '下一頁 &raquo;',
     'previous' => '&laquo; 上一頁',
-    'results' => '顯示 :from 到 :to 共 :total 個結果',
+    'results'  => '顯示 :from 到 :to 共 :total 個結果',
 ];
