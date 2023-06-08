728b0d476163f8fa9cfaaffc7d21c71eb48277b7
Patrick Her <renfu.her@gmail.com>
2023-06-08 21:27:04 +0800

    update index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -15,36 +15,51 @@ class IndexController extends Controller
     // 首頁
     public function index()
     {
-        $products = Product::inRandomOrder()->where('category_id', 1)->take(20)->get();
-        foreach($products as $key => $value){
-            if($value->define_image == 0){
-                $products[$key]->image_url = 'https://down-tw.img.susercontent.com/file/' .$value->image;
-            } else {
-                $products[$key]->image_url = asset('upload/images/' . $value->id . '/' . $value->image);
-            }
-        }
 
-        $products3 = Product::inRandomOrder()->where('category_id', 3)->take(20)->get();
-        foreach($products3 as $key => $value){
-            if($value->define_image == 0){
-                $products3[$key]->image_url = 'https://down-tw.img.susercontent.com/file/' .$value->image;
-            } else {
-                $products3[$key]->image_url = asset('upload/images/' . $value->id . '/' . $value->image);
-            }
-        }
+
+        // $products3 = Product::inRandomOrder()->where('category_id', 3)->take(20)->get();
+        // foreach ($products3 as $key => $value) {
+        //     if ($value->define_image == 0) {
+        //         $products3[$key]->image_url = 'https://down-tw.img.susercontent.com/file/' . $value->image;
+        //     } else {
+        //         $products3[$key]->image_url = asset('upload/images/' . $value->id . '/' . $value->image);
+        //     }
+        // }
 
         $product_images = ProductImage::all();
-        $product_categories = ProductCategory::all();
+
+        $products = [];
+        $product_categories = ProductCategory::orderBy('sort')->get();
+        foreach ($product_categories as $key => $value) {
+
+            $products[$key] = [];
+
+            $pd = Product::inRandomOrder()->where('category_id', $value->id)->take(20)->get();
+            if(count($pd) > 0){
+                foreach ($pd as $k => $v) {
+                    $products[$key][$k] = $v;
+                    if ($v->define_image == 0) {
+                        $products[$key][$k]['image_url'] = 'https://down-tw.img.susercontent.com/file/' . $v->image;
+                    } else {
+                        $products[$key][$k]['image_url'] = asset('upload/images/' . $v->id . '/' . $v->image);
+                    }
+                }
+            }
+
+        }
+
+        // dd($products);
+
         $banners = Banner::orderByDesc('id')->get();
 
-        return view('frontend.index',
-            compact('products',
-                    'product_images',
-                    'product_categories',
-                    'banners',
-                    'products3'
-                    )
+        return view(
+            'frontend.index',
+            compact(
+                'products',
+                'product_images',
+                'product_categories',
+                'banners',
+            )
         );
     }
-
 }
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,28 +7,30 @@
         <section class="position-relative bg-overlay-dark " style="height: 550px">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
-                    @foreach($banners as $key => $value)
-                    <div class="carousel-item @if($key == 0) active @endif">
-                        <img src="{{ asset('upload/banner/' . $value->image) }}" class="d-block w-100" style="object-fit: cover; width: 100%; height: 550px">
-                    </div>
+                    @foreach ($banners as $key => $value)
+                        <div class="carousel-item @if ($key == 0) active @endif">
+                            <img src="{{ asset('upload/banner/' . $value->image) }}" class="d-block w-100"
+                                style="object-fit: cover; width: 100%; height: 550px">
+                        </div>
                     @endforeach
 
                 </div>
             </div>
         </section>
 
-        <section class="mb-9 mt-5" data-aos="fade-up">
-            <div class="container">
-                <div class="w-md-50 mb-5">
-                    {{-- <p class="small fw-bolder text-uppercase tracking-wider mb-2 text-muted">Summer Favourites</p> --}}
-                    <h2 class="display-5 fw-bold mb-3">#304不鏽鋼白鐵專區</h2>
-                    {{-- <p class="lead">We've sorted through our feed to put together a collection of our products
+        @foreach ($product_categories as $key => $value)
+            <section class="mb-9 mt-5" data-aos="fade-up">
+                <div class="container">
+                    <div class="w-md-50 mb-5">
+                        {{-- <p class="small fw-bolder text-uppercase tracking-wider mb-2 text-muted">Summer Favourites</p> --}}
+                        <h3 class="fw-bold mb-3">{{ $value->name }}</h3>
+                        {{-- <p class="lead">We've sorted through our feed to put together a collection of our products
                         perfect
                         for a summer outdoors.</p> --}}
-                </div>
-                <!-- Swiper Latest -->
-                <div class="swiper-container overflow-visible" data-swiper
-                    data-options='{
+                    </div>
+                    <!-- Swiper Latest -->
+                    <div class="swiper-container overflow-visible" data-swiper
+                        data-options='{
                 "spaceBetween": 25,
                 "cssMode": true,
                 "roundLengths": true,
@@ -56,21 +58,21 @@
                   }
                 }
               }'>
-                    <div class="swiper-wrapper pb-5 pe-1">
-                        @foreach($products as $key => $value)
-                        <div class="swiper-slide d-flex h-auto">
-                            <!-- Card Product-->
-                            <div class="card position-relative h-100 card-listing hover-trigger">
-                                <div class="card-header">
-                                    <picture class="position-relative overflow-hidden d-block bg-light">
-                                        <img class="w-100 img-fluid position-relative z-index-10" title=""
-                                            src="{{ $value->image_url }}" alt="">
-                                    </picture>
-                                    <picture class="position-absolute z-index-20 start-0 top-0 hover-show bg-light">
-                                        <img class="w-100 img-fluid" title=""
-                                            src="{{ $value->image_url }}" alt="">
-                                    </picture>
-                                    {{-- <div class="card-actions">
+                        <div class="swiper-wrapper pb-5 pe-1">
+                            @forelse($products[$key] as $k => $v)
+                                <div class="swiper-slide d-flex h-auto">
+                                    <!-- Card Product-->
+                                    <div class="card position-relative h-100 card-listing hover-trigger">
+                                        <div class="card-header">
+                                            <picture class="position-relative overflow-hidden d-block bg-light">
+                                                <img class="w-100 img-fluid position-relative z-index-10" title=""
+                                                    src="{{ $v->image_url }}" alt="">
+                                            </picture>
+                                            <picture class="position-absolute z-index-20 start-0 top-0 hover-show bg-light">
+                                                <img class="w-100 img-fluid" title="" src="{{ $v->image_url }}"
+                                                    alt="">
+                                            </picture>
+                                            {{-- <div class="card-actions">
                                         <span
                                             class="small text-uppercase tracking-wide fw-bolder text-center d-block">Quick
                                             Add</span>
@@ -80,9 +82,9 @@
                                             <button class="btn btn-outline-dark btn-sm mx-2">L</button>
                                         </div>
                                     </div> --}}
-                                </div>
-                                <div class="card-body px-0 text-center">
-                                    {{-- <div class="d-flex justify-content-center align-items-center mx-auto mb-1">
+                                        </div>
+                                        <div class="card-body px-0 text-center">
+                                            {{-- <div class="d-flex justify-content-center align-items-center mx-auto mb-1">
                                         <!-- Review Stars Small-->
                                         <div class="rating position-relative d-table">
                                             <div class="position-absolute stars" style="width: 90%">
@@ -101,145 +103,39 @@
                                             </div>
                                         </div> <span class="small fw-bolder ms-2 text-muted"> 4.7 (456)</span>
                                     </div> --}}
-                                    <a class="mb-0 mx-2 mx-md-4 fs-p link-cover text-decoration-none d-block text-center"
-                                        href="/product/{{ $value->id }}">{{ $value->name }}</a>
-                                    <p class="fw-bolder m-0 mt-2">$ {{ $value->price }}</p>
-                                </div>
-                            </div>
-                            <!--/ Card Product-->
-                        </div>
-                        @endforeach
-                    </div>
-
-                    <!-- Buttons-->
-                    <div
-                        class="swiper-btn swiper-disabled-hide swiper-prev swiper-btn-side btn-icon bg-dark text-white ms-3 shadow-lg mt-n5 ms-n4">
-                        <i class="ri-arrow-left-s-line ri-lg"></i>
-                    </div>
-                    <div
-                        class="swiper-btn swiper-disabled-hide swiper-next swiper-btn-side swiper-btn-side-right btn-icon bg-dark text-white me-n4 shadow-lg mt-n5">
-                        <i class="ri-arrow-right-s-line ri-lg"></i>
-                    </div>
-
-                    <!-- Add Scrollbar -->
-                    <div class="swiper-scrollbar"></div>
-
-                </div>
-                <!-- / Swiper Latest-->
-            </div>
-        </section>
-
-        <section class="mb-9 mt-5" data-aos="fade-up">
-            <div class="container">
-                <div class="w-md-50 mb-5">
-                    {{-- <p class="small fw-bolder text-uppercase tracking-wider mb-2 text-muted">Summer Favourites</p> --}}
-                    <h2 class="display-5 fw-bold mb-3">各式尺寸鍍鋅籠</h2>
-                    {{-- <p class="lead">We've sorted through our feed to put together a collection of our products
-                        perfect
-                        for a summer outdoors.</p> --}}
-                </div>
-                <!-- Swiper Latest -->
-                <div class="swiper-container overflow-visible" data-swiper
-                    data-options='{
-                "spaceBetween": 25,
-                "cssMode": true,
-                "roundLengths": true,
-                "scrollbar": {
-                  "el": ".swiper-scrollbar",
-                  "hide": false,
-                  "draggable": true
-                },
-                "navigation": {
-                  "nextEl": ".swiper-next",
-                  "prevEl": ".swiper-prev"
-                },
-                "breakpoints": {
-                  "576": {
-                    "slidesPerView": 1
-                  },
-                  "768": {
-                    "slidesPerView": 2
-                  },
-                  "992": {
-                    "slidesPerView": 3
-                  },
-                  "1200": {
-                    "slidesPerView": 4
-                  }
-                }
-              }'>
-                    <div class="swiper-wrapper pb-5 pe-1">
-                        @foreach($products3 as $key => $value)
-                        <div class="swiper-slide d-flex h-auto">
-                            <!-- Card Product-->
-                            <div class="card position-relative h-100 card-listing hover-trigger">
-                                <div class="card-header">
-                                    <picture class="position-relative overflow-hidden d-block bg-light">
-                                        <img class="w-100 img-fluid position-relative z-index-10" title=""
-                                            src="{{ $value->image_url }}" alt="">
-                                    </picture>
-                                    <picture class="position-absolute z-index-20 start-0 top-0 hover-show bg-light">
-                                        <img class="w-100 img-fluid" title=""
-                                            src="{{ $value->image_url }}" alt="">
-                                    </picture>
-                                    {{-- <div class="card-actions">
-                                        <span
-                                            class="small text-uppercase tracking-wide fw-bolder text-center d-block">Quick
-                                            Add</span>
-                                        <div class="d-flex justify-content-center align-items-center flex-wrap mt-3">
-                                            <button class="btn btn-outline-dark btn-sm mx-2">S</button>
-                                            <button class="btn btn-outline-dark btn-sm mx-2">M</button>
-                                            <button class="btn btn-outline-dark btn-sm mx-2">L</button>
+                                            <a class="mb-0 mx-2 mx-md-4 fs-p link-cover text-decoration-none d-block text-center"
+                                                href="/product/{{ $v->id }}">{{ $v->name }}</a>
+                                            <p class="fw-bolder m-0 mt-2">$ {{ $v->price }}</p>
                                         </div>
-                                    </div> --}}
+                                    </div>
+                                    <!--/ Card Product-->
                                 </div>
-                                <div class="card-body px-0 text-center">
-                                    {{-- <div class="d-flex justify-content-center align-items-center mx-auto mb-1">
-                                        <!-- Review Stars Small-->
-                                        <div class="rating position-relative d-table">
-                                            <div class="position-absolute stars" style="width: 90%">
-                                                <i class="ri-star-fill text-dark mr-1"></i>
-                                                <i class="ri-star-fill text-dark mr-1"></i>
-                                                <i class="ri-star-fill text-dark mr-1"></i>
-                                                <i class="ri-star-fill text-dark mr-1"></i>
-                                                <i class="ri-star-fill text-dark mr-1"></i>
-                                            </div>
-                                            <div class="stars">
-                                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                                <i class="ri-star-fill mr-1 text-muted opacity-25"></i>
-                                            </div>
-                                        </div> <span class="small fw-bolder ms-2 text-muted"> 4.7 (456)</span>
-                                    </div> --}}
-                                    <a class="mb-0 mx-2 mx-md-4 fs-p link-cover text-decoration-none d-block text-center"
-                                        href="/product/{{ $value->id }}">{{ $value->name }}</a>
-                                    <p class="fw-bolder m-0 mt-2">$ {{ $value->price }}</p>
-                                </div>
+                            @empty
+                            <div class="swiper-slide text-center w-100" style="height: 150px; border: 1px solid; line-height: 150px;" >
+                                尚未有商品
                             </div>
-                            <!--/ Card Product-->
+                            @endforelse
                         </div>
-                        @endforeach
-                    </div>
 
-                    <!-- Buttons-->
-                    <div
-                        class="swiper-btn swiper-disabled-hide swiper-prev swiper-btn-side btn-icon bg-dark text-white ms-3 shadow-lg mt-n5 ms-n4">
-                        <i class="ri-arrow-left-s-line ri-lg"></i>
-                    </div>
-                    <div
-                        class="swiper-btn swiper-disabled-hide swiper-next swiper-btn-side swiper-btn-side-right btn-icon bg-dark text-white me-n4 shadow-lg mt-n5">
-                        <i class="ri-arrow-right-s-line ri-lg"></i>
-                    </div>
+                        <!-- Buttons-->
+                        <div
+                            class="swiper-btn swiper-disabled-hide swiper-prev swiper-btn-side btn-icon bg-dark text-white ms-3 shadow-lg mt-n5 ms-n4">
+                            <i class="ri-arrow-left-s-line ri-lg"></i>
+                        </div>
+                        <div
+                            class="swiper-btn swiper-disabled-hide swiper-next swiper-btn-side swiper-btn-side-right btn-icon bg-dark text-white me-n4 shadow-lg mt-n5">
+                            <i class="ri-arrow-right-s-line ri-lg"></i>
+                        </div>
 
-                    <!-- Add Scrollbar -->
-                    <div class="swiper-scrollbar"></div>
+                        <!-- Add Scrollbar -->
+                        <div class="swiper-scrollbar"></div>
 
+                    </div>
+                    <!-- / Swiper Latest-->
                 </div>
-                <!-- / Swiper Latest-->
-            </div>
-        </section>
+            </section>
+        @endforeach
+
         <!-- / Staff Picks-->
 
         <!-- Image Hotspot Banner-->
