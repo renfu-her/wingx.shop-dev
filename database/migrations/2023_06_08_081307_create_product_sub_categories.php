55cc3a5f1d7d160032e9632b1b90fa4e045c795b
Patrick Her <renfu.her@gmail.com>
2023-06-08 08:15:17 +0800

    migrate model

---
+++ database/migrations/2023_06_08_081307_create_product_sub_categories.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->comment('產品ID');
+            $table->bigInteger('product_id_sub2')->comment('產品ID 2');
+            $table->bigInteger('product_id_sub3')->comment('產品ID 2');
+            $table->longText('description')->comment('產品描述');
+            $table->integer('status')->default(1)->comment('狀態 1:啟用 2:停用');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_sub_categories');
+    }
+};
