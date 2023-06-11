8d626f505e7bd0838dc9bebf300d97299c3b52c6
Patrick Her <renfu.her@gmail.com>
2023-06-11 18:14:30 +0800

    update css

--- resources/views/frontend/layouts/base.blade.php
+++ resources/views/frontend/layouts/base.blade.php
@@ -31,6 +31,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('frontend/css/login-register.css') }}">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css?t=' . time()) }}" />
     <!-- Fix for custom scrollbar if JS is disabled-->
     <noscript>
         <style>
