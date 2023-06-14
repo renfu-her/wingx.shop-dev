598fc798b2e67bc2a64c01245dc3763749c172ea
Patrick Her <renfu.her@gmail.com>
2023-06-14 11:44:16 +0800

    update orders model

---
+++ database/migrations/2023_06_14_114156_change_ship_date_to_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->datetime('ship_date')->change();
+            $table->string('county')->nullable()->comment('縣市');
+            $table->string('district')->nullable()->comment('區域');
+            $table->string('zipcode')->nullable()->comment('zip code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
