921e14a52af228a0b74afe96c30871e09b642fb8
Patrick Her <renfu.her@gmail.com>
2023-06-14 10:43:43 +0800

    add tw-zipcode

---
+++ database/migrations/2023_06_14_104115_add_columns_members_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('county')->nullable()->comment('縣市');
+            $table->string('district')->nullable()->comment('區域');
+            $table->string('zipcode')->nullable()->comment('zip code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            //
+        });
+    }
+};
