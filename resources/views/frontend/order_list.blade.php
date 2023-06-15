ad4ff761a4c79ce2aab4db71fcfdfaff52bb20a5
Patrick Her <renfu.her@gmail.com>
2023-06-15 20:36:38 +0800

    update order_list

--- resources/views/frontend/order_list.blade.php
+++ resources/views/frontend/order_list.blade.php
@@ -9,66 +9,71 @@
                         <div class="text-center">
                             <h5>訂單列表</h5>
                         </div>
-                        @foreach ($orders as $key => $row)
+                        @foreach ($orders as $key => $value)
                             <div class="container container-2">
-                                <header class="d-flex justify-content-between align-items-center">
-                                    <div class="row">
-                                        <div class="col-6 col-md-12 text">
-                                            <span>訂單編號：{{ $row->order_no }}</span>
-                                        </div>
-                                        <div class="col-10">
-                                            <div class="col-6 col-md-12 text">
-                                                <span>訂單成立：{{ $row->created_at }}</span>
-                                                <span>付款方式：{{ $row->payment_name }}</span>
+                                <div class="col-12">
+                                    <div class="col-12">
+                                        <span>訂單編號：{{ $value->order_no }}</span>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="col-12 hr-line">
+                                        <div class="row">
+                                            <div class="col-12 col-md-6">
+                                                <span>訂單成立：{{ $value->created_at }}</span>
                                             </div>
-                                        </div>
-                                        <div class="col-10">
-                                            <div class="col-6 col-md-12 text">
-                                                <span
-                                                    style="margin-right: @if (trim($row->pay_time) == '') 205px @else 60px @endif">付款時間：{{ $row->pay_time }}</span>
-                                                <span>
-                                                    金額：NT ${{ $row->total }} <br>
-                                                    運費：NT ${{ $row->ship_price }} <br>
-                                                    實付金額：NT ${{ $row->ttl_price }}
-                                                    @if ($row->status == 0 || $row->status == 9)
-                                                        <span class="text-icon">待付款</span>
-                                                    @elseif($row->status == 1)
-                                                        <span class="text-icon">
-                                                            <i class="bx bx-checkbox-checked"></i>完成付款</span>
-                                                    @elseif($row->status == 2)
-                                                        <span class="text-icon">付款失敗</span>
-                                                    @elseif($row->status == 3)
-                                                        <span class="text-icon">取消訂單</span>
-                                                    @endif
-                                                </span>
+                                            <div class="col-12 col-md-6">
+                                                <span>付款方式：{{ $value->payment_name }}</span>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="check col-2 col-md-2">
-                                        @if ($row->status == 0 || $row->status == 9)
-                                            <span class="text-icon">待付款</span>
-                                        @elseif($row->status == 1)
-                                            <i class='bx bx-checkbox-checked'></i><span>完成付款</span>
-                                        @elseif($row->status == 2)
-                                            <span class="text-icon">付款失敗</span>
-                                        @elseif($row->status == 3)
-                                            <span class="text-icon">取消訂單</span>
-                                        @endif
-                                    </div>
-                                </header>
+                                </div>
+                                <div class="col-12">
+                                    <div class="col-12">
+                                        <div class="row">
+                                            <div class="col-12 col-md-3">
+                                                運費：NT ${{ $value->ship_price }} <br>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                金額：NT ${{ $value->total }} <br>
 
-                                @foreach ($row['order_detail'] as $dkey => $drow)
-                                    <div class="nt">
-                                        <div class="image d-flex align-items-center justify-content-between">
-                                            <img src="{{ $drow->image_url }}" loading="lazy">
-                                            <span>{{ $drow->title }}</span>
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                實付金額：NT ${{ $value->ttl_price }} <br>
+
+                                            </div>
+                                            <div class="col-12 col-md-3">
+                                                @if ($value->status == 0 || $value->status == 9)
+                                                    <span class="text-icon">待付款</span>
+                                                @elseif($value->status == 1)
+                                                    <span class="text-icon">完成付款</span>
+                                                @elseif($value->status == 2)
+                                                    <span class="text-icon">付款失敗</span>
+                                                @elseif($value->status == 3)
+                                                    <span class="text-icon">取消訂單</span>
+                                                @endif
+                                            </div>
                                         </div>
-                                        <div class="num">數量: {{ $drow->qty }}</div>
-                                        <div class="num">NT ${{ $drow->price }}</div>
+                                    </div>
+                                </div>
+
+                                @foreach ($value['order_detail'] as $dkey => $drow)
+                                    <div class="row">
+                                        <div class="col-6 col-md-3">
+                                            <img src="{{ $drow->image_url }}" style="height: 150px" loading="lazy">
+                                        </div>
+                                        <div class="col-6 col-md-3">
+                                            <span>{{ $drow->name }}</span>
+                                        </div>
+                                        <div class="col-6 col-md-3">
+                                            <span>數量：{{ $drow->qty }}</span>
+                                        </div>
+                                        <div class="col-6 col-md-3">
+                                            <span>單價：NT ${{ $drow->price }}</span>
+                                        </div>
                                     </div>
                                 @endforeach
                             </div>
-                            <div class="hr-line"></div>
                         @endforeach
                     </div>
                 </div>
