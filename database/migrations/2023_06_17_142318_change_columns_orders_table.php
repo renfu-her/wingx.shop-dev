0b50564dd6e6c49eb47d06a6e0ba7dc60a1cf96c
Patrick Her <renfu.her@gmail.com>
2023-06-17 16:48:55 +0800

    update orders model

---
+++ database/migrations/2023_06_17_142318_change_columns_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('amount')->default(0)->comment('總金額');
+            $table->integer('tax')->default(0)->comment('稅額');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
