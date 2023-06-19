17061032ae4bdcadb3d04c834f57a14a3b5d362c
Patrick Her <renfu.her@gmail.com>
2023-06-19 15:09:25 +0800

    update index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -28,95 +28,80 @@
             </button>
         </section>
 
-        @foreach ($product_categories as $key => $value)
-            <section class="mb-9 mt-5" data-aos="fade-up">
-                <div class="container">
-                    <div class="w-md-50 mb-5">
-                        {{-- <p class="small fw-bolder text-uppercase tracking-wider mb-2 text-muted">Summer Favourites</p> --}}
-                        <h3 class="fw-bold mb-3">{{ $value->name }}</h3>
-                        {{-- <p class="lead">We've sorted through our feed to put together a collection of our products
-                        perfect
-                        for a summer outdoors.</p> --}}
+        <div class="container">
+            <div class="row">
+                @if ($product_categories[0])
+                    <div class="col-12 col-md-4 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-1.jpg') }}) 0% 0% / cover no-repeat;">
+                            <h5 style="color: white">{{ $product_categories[0]['name'] }}</h5>
+                            <a href="/category/1" class="btn btn-dar" style="background-color: #FF5F42 !important">購物去</a>
+                        </div>
                     </div>
-                    <!-- Swiper Latest -->
-                    <div class="swiper-container overflow-visible" data-swiper
-                        data-options='{
-                "spaceBetween": 25,
-                "cssMode": true,
-                "roundLengths": true,
-                "scrollbar": {
-                  "el": ".swiper-scrollbar",
-                  "hide": false,
-                  "draggable": true
-                },
-                "navigation": {
-                  "nextEl": ".swiper-next",
-                  "prevEl": ".swiper-prev"
-                },
-                "breakpoints": {
-                  "576": {
-                    "slidesPerView": 1
-                  },
-                  "768": {
-                    "slidesPerView": 2
-                  },
-                  "992": {
-                    "slidesPerView": 3
-                  },
-                  "1200": {
-                    "slidesPerView": 4
-                  }
-                }
-              }'>
-                        <div class="swiper-wrapper pb-5 pe-1">
-                            @forelse($products[$key] as $k => $v)
-                                <div class="swiper-slide d-flex h-auto">
-                                    <!-- Card Product-->
-                                    <div class="card position-relative h-100 card-listing hover-trigger">
-                                        <div class="card-header">
-                                            <picture class="position-relative overflow-hidden d-block bg-light">
-                                                <img class="w-100 img-fluid position-relative z-index-10" title=""
-                                                    src="{{ $v->image_url }}" alt="">
-                                            </picture>
-                                            <picture class="position-absolute z-index-20 start-0 top-0 hover-show bg-light">
-                                                <img class="w-100 img-fluid" title="" src="{{ $v->image_url }}"
-                                                    alt="">
-                                            </picture>
-                                        </div>
-                                        <div class="card-body px-0 text-center">
-                                            <a class="mb-0 mx-2 mx-md-4 fs-p link-cover text-decoration-none d-block text-center"
-                                                href="/product/{{ $v->id }}">{{ $v->name }}</a>
-                                            <p class="fw-bolder m-0 mt-2">$ {{ $v->price }}</p>
-                                        </div>
-                                    </div>
-                                    <!--/ Card Product-->
-                                </div>
-                            @empty
-                                <div class="swiper-slide text-center w-100"
-                                    style="height: 150px; border: 1px solid; line-height: 150px;">
-                                    尚未有商品
-                                </div>
-                            @endforelse
+                @endif
+                @if ($product_categories[1])
+                    <div class="col-12 col-md-8 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-2.jpg') }}) 0% 0% / cover no-repeat">
+                            <h5 style="color: white">{{ $product_categories[1]['name'] }}</h5>
+                            <a href="/category/2" class="btn btn-dark"  style="background-color: #FF5F42 !important">購物去</a>
                         </div>
-
-                        <!-- Buttons-->
-                        <div
-                            class="swiper-btn swiper-disabled-hide swiper-prev swiper-btn-side btn-icon bg-dark text-white ms-3 shadow-lg mt-n5 ms-n4">
-                            <i class="ri-arrow-left-s-line ri-lg"></i>
-                        </div>
-                        <div
-                            class="swiper-btn swiper-disabled-hide swiper-next swiper-btn-side swiper-btn-side-right btn-icon bg-dark text-white me-n4 shadow-lg mt-n5">
-                            <i class="ri-arrow-right-s-line ri-lg"></i>
-                        </div>
-
-                        <!-- Add Scrollbar -->
-                        <div class="swiper-scrollbar"></div>
-
                     </div>
-                    <!-- / Swiper Latest-->
-                </div>
-            </section>
-        @endforeach
+                @endif
+            </div>
+            <div class="row">
+                @if ($product_categories[2])
+                    <div class="col-12 col-md-8 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-3.jpg') }}) 0% 0% / cover no-repeat;">
+                            <h5 style="color: white">{{ $product_categories[2]['name'] }}</h5>
+                            <a href="/category/1" class="btn btn-dar" style="background-color: #FF5F42 !important">購物去</a>
+                        </div>
+                    </div>
+                @endif
+                @if ($product_categories[3])
+                    <div class="col-12 col-md-4 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-4.jpg') }}) 0% 0% / cover no-repeat">
+                            <h5 style="color: white">{{ $product_categories[3]['name'] }}</h5>
+                            <a href="/category/2" class="btn btn-dark"  style="background-color: #FF5F42 !important">購物去</a>
+                        </div>
+                    </div>
+                @endif
+            </div>
+            <div class="row">
+                @if ($product_categories[4])
+                    <div class="col-12 col-md-4 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-4.jpg') }}) 0% 0% / cover no-repeat;">
+                            <h5 style="color: white">{{ $product_categories[4]['name'] }}</h5>
+                            <a href="/category/1" class="btn btn-dar" style="background-color: #FF5F42 !important">購物去</a>
+                        </div>
+                    </div>
+                @endif
+                @if ($product_categories[5])
+                    <div class="col-12 col-md-8 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-5.jpg') }}) 0% 0% / cover no-repeat">
+                            <h5 style="color: white">{{ $product_categories[5]['name'] }}</h5>
+                            <a href="/category/2" class="btn btn-dark"  style="background-color: #FF5F42 !important">購物去</a>
+                        </div>
+                    </div>
+                @endif
+            </div>
+            <div class="row">
+                @if ($product_categories[6])
+                    <div class="col-12 col-md-8 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-6.jpg') }}) 0% 0% / cover no-repeat;">
+                            <h5 style="color: white">{{ $product_categories[6]['name'] }}</h5>
+                            <a href="/category/1" class="btn btn-dar" style="background-color: #FF5F42 !important">購物去</a>
+                        </div>
+                    </div>
+                @endif
+                @if ($product_categories[7])
+                    <div class="col-12 col-md-4 pt-6">
+                        <div class="p-8" style="background: url({{ asset('frontend/images/home/index-slide-7.jpg') }}) 0% 0% / cover no-repeat">
+                            <h5 style="color: white">{{ $product_categories[7]['name'] }}</h5>
+                            <a href="/category/2" class="btn btn-dark"  style="background-color: #FF5F42 !important">購物去</a>
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
 
     </section>
     <!-- / Main Section-->
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -13,9 +13,7 @@
     <meta name="keywords" content="">
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('frontend/favicon/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('frontend/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('frontend/favicon/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('frontend/images/favicon.png') }}">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
