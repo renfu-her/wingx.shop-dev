20fa0cec266344f6f159ccd21a56b4304abe5692
Patrick Her <renfu.her@gmail.com>
2023-06-21 10:10:08 +0800

    profile

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -96,22 +96,6 @@
                 let mobile = $('input[name=mobile]').val();
                 let name = $('input[name=name]').val();
 
-                console.log(password);
-
-                if($.trim(password) != ''){
-                    password_regex = /^[a-zA-Z0-9]{6,}$/i;
-
-                    consoe.log(password);
-
-                    console.log(password.search(password_regex));
-
-                    // if(password.search(password_regex) == -1){
-                    //     error_msg.push("請輸入6 位以上，密碼必須包含字母以及數字組成");
-                    // }
-
-
-                }
-
                 if($.trim(name) == ''){
                     error_msg.push("請輸入名稱");
                 }
@@ -123,8 +107,6 @@
                 if($.trim(address) == ''){
                     error_msg.push("請輸入地址");
                 }
-
-                return false;
 
                 if(error_msg.length > 0){
                     alert(error_msg.join("\n"));
