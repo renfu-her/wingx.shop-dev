6aa405a0f0e2ed8ff8066f3ea1dca364c5cb3723
Patrick Her <renfu.her@gmail.com>
2023-07-05 11:07:29 +0800

    update order status command

--- app/Console/Commands/OrderStatus.php
+++ app/Console/Commands/OrderStatus.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use TsaiYiHua\ECPay\QueryTradeInfo;
 
 use App\Models\Order;
 
@@ -12,6 +13,13 @@ class OrderStatus extends Command
 
     protected $signature = 'OrderStatus';
     protected $description = '訂單狀態更新';
+    protected $queryTradeInfo;
+
+    public function __construct(QueryTradeInfo $queryTradeInfo)
+    {
+        parent::__construct();
+        $this->queryTradeInfo = $queryTradeInfo;
+    }
 
     // 訂單狀態更新
     public function handle()
@@ -21,7 +29,9 @@ class OrderStatus extends Command
         $orders = Order::whereIn('status', [0, 9])->get();
         foreach ($orders as $key => $value) {
 
-            
+            $order_status = $this->queryTradeInfo->getData($value->order_no)->query();
+            Log::info($order_status);
+
         }
         Log::info('=== 訂單狀態更新完成 ===');
     }
