0e14dc4c4c50b19eb2f83ced4fc49ded34224e6c
Patrick Her <renfu.her@gmail.com>
2023-07-05 11:14:44 +0800

    update order status

--- app/Console/Commands/OrderStatus.php
+++ app/Console/Commands/OrderStatus.php
@@ -13,13 +13,6 @@ class OrderStatus extends Command
 
     protected $signature = 'OrderStatus';
     protected $description = '訂單狀態更新';
-    protected $queryTradeInfo;
-
-    public function __construct(QueryTradeInfo $queryTradeInfo)
-    {
-        parent::__construct();
-        $this->queryTradeInfo = $queryTradeInfo;
-    }
 
     // 訂單狀態更新
     public function handle()
@@ -29,28 +22,12 @@ class OrderStatus extends Command
         $orders = Order::whereIn('status', [0, 9])->get();
         foreach ($orders as $key => $value) {
 
-            $order_status = $this->queryTradeInfo->getData($value->order_no)->query();
+            $order_status = (new QueryTradeInfo())->getData($value->order_no)->query();
             Log::info($order_status);
 
         }
         Log::info('=== 訂單狀態更新完成 ===');
     }
 
-    // check value
-    public function check_value($amt, $mno)
-    {
-        $mid = env("NEWEBPAY_MERCHANT_ID");
-        $key = env("NEWEBPAY_HASH_KEY");
-        $iv = env("NEWEBPAY_HASH_IV");
-
-        $data1 = http_build_query(array(
-            'Amt' => $amt,
-            'MerchantID' => $mid,
-            'MerchantOrderNo' => $mno
-        ));
-        $hashs = "IV=" . $iv . "&" . $data1 . "&Key=" . $key;
-        $hash = strtoupper(hash("sha256", $hashs));
-
-        return $hash;
-    }
+    
 }
