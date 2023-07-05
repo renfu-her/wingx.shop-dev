d40d1c6f3378961ee80ed168249fc63b44ccd8d0
Patrick Her <renfu.her@gmail.com>
2023-07-05 10:52:54 +0800

    update order status

--- app/Console/Commands/OrderStatus.php
+++ app/Console/Commands/OrderStatus.php
@@ -21,39 +21,7 @@ class OrderStatus extends Command
         $orders = Order::whereIn('status', [0, 9])->get();
         foreach ($orders as $key => $value) {
 
-            $amt = $value['total'] + $value['ship_price'];
-            $mno = $value['order_no'];
-            $mid = env("NEWEBPAY_MERCHANT_ID");
-            $check_value = $this->check_value($amt, $mno);
-
-            $url = "https://core.newebpay.com/API/QueryTradeInfo";
-            if (config('config.APP_ENV') == 'dev') {
-                $url = "https://ccore.newebpay.com/API/QueryTradeInfo";
-            }
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(
-                array(
-                    "Version" => "1.3", "MerchantID" => $mid, "RespondType" => "JSON",
-                    "CheckValue" => $check_value, "TimeStamp" => time(),
-                    "MerchantOrderNo" => $value['order_no'], "Amt" => $amt
-                )
-            ));
-            $output = curl_exec($ch);
-            curl_close($ch);
-
-            $result = json_decode($output, true);
-            if($result['Status'] == 'SUCCESS'){
-                $res = $result['Result'];
-                $orderStatus = $res['TradeStatus'];
-                // 更新訂單狀態
-                Log::info('=== 訂單狀態更新 MerchantOrderNo: ' . $res['MerchantOrderNo'] . ' ===');
-                Order::where('order_no', $res['MerchantOrderNo'])->update(['status' => $orderStatus]);
-
-            }
+            
         }
         Log::info('=== 訂單狀態更新完成 ===');
     }
