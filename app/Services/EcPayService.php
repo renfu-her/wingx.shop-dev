aacac1f7471c3bd22a2c8023901a8c2298b06a0d
Patrick Her <renfu.her@gmail.com>
2023-07-06 09:01:09 +0800

    update invoice test

--- app/Services/EcPayService.php
+++ app/Services/EcPayService.php
@@ -30,141 +30,91 @@ class EcPayService extends BaseService
             $hashIV = config('config.INVOCE_HASH_IV');
         }
 
-
-        // $factory = new Factory([
-        //     'hashKey' => $hashKey,
-        //     'hashIv' => $hashIV,
-        // ]);
-
-        // $postService = $factory->create('PostWithAesJsonResponseService');
-
-        // $order = Order::where('order_no', $order_no)->first();
-
-        // $order_price = $order->amount + $order->tax;
-
-        // $data = [
-        //     'MerchantID' => $merchantId,
-        //     'RelateNumber' => 'INV-' . date('YmdHis'),
-        //     'CustomerID' => '',
-        //     'CustomerAddr' => $order->company_address,
-        //     'CustomerPhone' => $order->mobile,
-        //     'CustomerEmail' => $order->email,
-        //     "ClearanceMark" => "1",
-        //     "Print" => "1",
-        //     'Donation' => "0",
-        //     'LoveCode' => '',
-        //     'CarrierType' => '',
-        //     'CarrierNum' => '',
-        //     'TaxType' => 1,
-        //     'SalesAmount' => (int)$order_price,
-        //     'InvType' => '07',
-        //     'vat' => 1,
-        // ];
-
-        // if(!empty($order->company_uid)){
-        //     $data['CustomerIdentifier'] = $order->company_id;
-        //     $data['CustomerName'] = $order->company_name;
-        // } else {
-        //     $data["CustomerIdentifier"] = "";
-        //     $data['CustomerName'] = $order->name;
-        // }
-
-        // $data['Items'] = [
-        //     [
-        //         'ItemSeq' => 1,
-        //         'ItemName' => '商品',
-        //         'ItemCount' => 1,
-        //         'ItemWord' => '個',
-        //         'ItemPrice' => $order_price,
-        //         'ItemTaxType' => 1,
-        //         'ItemAmount' => $order_price,
-        //         'ItemRemark' => '',
-        //     ],
-        // ];
-
-        // $itemCount = 1;
-        // $itemPriceIncludeTax = 100;
-        // $itemAmount = round(($itemPriceIncludeTax * $itemCount), 0);
-        // $saleAmount = $itemAmount;
-
-        // $data = [
-        //     'MerchantID' => $merchantId,
-        //     'RelateNumber' => 'Test' . time(),
-        //     'CustomerPhone' => '0911222333',
-        //     'Print' => '0',
-        //     'Donation' => '0',
-        //     'CarrierType' => '1',
-        //     'TaxType' => '1',
-        //     'SalesAmount' => $saleAmount,
-        //     'Items' => [
-        //         [
-        //             'ItemName' => '測試商品01',
-        //             'ItemCount' => $itemCount,
-        //             'ItemWord' => '個',
-        //             'ItemPrice' => $itemPriceIncludeTax,
-        //             'ItemTaxType' => '1',
-        //             'ItemAmount' => $itemAmount,
-        //         ],
-        //     ],
-        //     'InvType' => '07'
-        // ];
-
-
-        // $input = [
-        //     'MerchantID' => $merchantId,
-        //     'RqHeader' => [
-        //         'Timestamp' => time(),
-        //         'Revision' => '3.0.0',
-        //     ],
-        //     'Data' => $data,
-        // ];
-
-        // dd($input, $data, $merchantId, $hashKey, $hashIV, $url);
-
         $factory = new Factory([
-            'hashKey' => 'ejCk326UnaZWKisg',
-            'hashIv' => 'q9jcZX8Ib9LM8wYk',
+            'hashKey' => $hashKey,
+            'hashIv' => $hashIV,
         ]);
         $postService = $factory->create('PostWithAesJsonResponseService');
 
         $order = Order::where('order_no', $order_no)->first();
 
-        $itemCount = 1;
-        $itemPriceIncludeTax = $order->amount + $order->tax;
-        $itemAmount = round(($itemPriceIncludeTax * $itemCount), 0);
-        $saleAmount = $itemAmount;
-        $data = [
-            'MerchantID' => '2000132',
-            'RelateNumber' => 'INV-' . time(),
-            'CustomerPhone' => '0911222333',
-            'Print' => '0',
-            'Donation' => '0',
-            'CarrierType' => '1',
-            'TaxType' => '1',
-            'SalesAmount' => $saleAmount,
-            'Items' => [
-                [
-                    'ItemName' => '商品',
-                    'ItemCount' => $itemCount,
-                    'ItemWord' => '個',
-                    'ItemPrice' => $itemPriceIncludeTax,
-                    'ItemTaxType' => '1',
-                    'ItemAmount' => $itemAmount,
+        if($order->company_uid){
+            $itemCount = 1;
+            $itemPrice = $order->total + $order->ship_price;
+            $itemAmount = ($itemPrice * $itemCount);
+            $saleAmount = $itemAmount;
+            $taxAmount = round(($saleAmount * 0.05), 0);
+            $data = [
+                'MerchantID' => $merchantId,
+                'RelateNumber' => 'INV-' . time(),
+                'CustomerName' => $order->company_name,
+                'CustomerIdentifier' => $order->company_uid,
+                'CustomerEmail' => $order->email,
+                'InvType' => '07',
+                'TaxType' => '1',
+                'Items' => [
+                    [
+                        'ItemSeq' => 1,
+                        'ItemName' => '測試商品01',
+                        'ItemCount' => $itemCount,
+                        'ItemPrice' => $itemPrice,
+                        'ItemTaxType' => '1',
+                        'ItemAmount' => $itemAmount,
+                    ],
                 ],
-            ],
-            'InvType' => '07'
-        ];
-        $input = [
-            'MerchantID' => '2000132',
-            'RqHeader' => [
-                'Timestamp' => time(),
-                'Revision' => '3.0.0',
-            ],
-            'Data' => $data,
-        ];
-        $url = 'https://einvoice-stage.ecpay.com.tw/B2CInvoice/Issue';
+                'SalesAmount' => $saleAmount,
+                'TaxAmount' => $taxAmount,
+                'TotalAmount' => ($saleAmount + $taxAmount),\
+            ];
+            $input = [
+                'MerchantID' => $merchantId,
+                'RqHeader' => [
+                    'Timestamp' => time(),
+                    'Revision' => '1.0.0',
+                ],
+                'Data' => $data,
+            ];
+            $url = 'https://einvoice-stage.ecpay.com.tw/B2BInvoice/Issue';
+        } else {
+            $itemCount = 1;
+            $itemPriceIncludeTax = $order->amount + $order->tax;
+            $itemAmount = round(($itemPriceIncludeTax * $itemCount), 0);
+            $saleAmount = $itemAmount;
+            $data = [
+                'MerchantID' => '2000132',
+                'RelateNumber' => 'INV-' . time(),
+                'CustomerPhone' => '0911222333',
+                'Print' => '0',
+                'Donation' => '0',
+                'CarrierType' => '1',
+                'TaxType' => '1',
+                'SalesAmount' => $saleAmount,
+                'Items' => [
+                    [
+                        'ItemName' => '商品',
+                        'ItemCount' => $itemCount,
+                        'ItemWord' => '個',
+                        'ItemPrice' => $itemPriceIncludeTax,
+                        'ItemTaxType' => '1',
+                        'ItemAmount' => $itemAmount,
+                    ],
+                ],
+                'InvType' => '07'
+            ];
+            $input = [
+                'MerchantID' => '2000132',
+                'RqHeader' => [
+                    'Timestamp' => time(),
+                    'Revision' => '3.0.0',
+                ],
+                'Data' => $data,
+            ];
+            $url = 'https://einvoice-stage.ecpay.com.tw/B2CInvoice/Issue';
 
-        // dd($data, $input, $url);
+        }
+
+
+        dd($data, $input, $url);
 
         $response = $postService->post($input, $url);
         var_dump($response);
