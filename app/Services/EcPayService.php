f8b31635bc8e5acad91996680b037712ca6f1612
Patrick Her <renfu.her@gmail.com>
2023-07-06 11:33:57 +0800

    e-invoice test

--- app/Services/EcPayService.php
+++ app/Services/EcPayService.php
@@ -18,7 +18,7 @@ class EcPayService extends BaseService
 
     public function ecpayInvoice($order_no)
     {
-        if(config('config.APP_ENV') == 'dev'){
+        if (config('config.APP_ENV') == 'dev') {
             $url = "https://einvoice-stage.ecpay.com.tw/B2CInvoice/Issue";
             $merchantId = config('config.INVOICE_ID_DEV');
             $hashKey = config('config.INVOICE_HASH_KEY_DEV');
@@ -38,7 +38,7 @@ class EcPayService extends BaseService
 
         $order = Order::where('order_no', $order_no)->first();
 
-        if($order->company_uid){
+        if ($order->company_uid) {
             $itemCount = 1;
             $itemPrice = $order->amount;
             $itemAmount = ($itemPrice * $itemCount);
@@ -109,15 +109,47 @@ class EcPayService extends BaseService
                 'Data' => $data,
             ];
             $url = 'https://einvoice-stage.ecpay.com.tw/B2CInvoice/Issue';
-
         }
 
-        // dd($data, $input, $url);
+        $itemCount = 3;
+        $itemPriceIncludeTax = 10.5;
+        $itemAmount = round(($itemPriceIncludeTax * $itemCount), 0);
+        $saleAmount = $itemAmount;
+        $data = [
+            'MerchantID' => '2000132',
+            'RelateNumber' => 'Test' . time(),
+            'CustomerPhone' => '0911222333',
+            'Print' => '0',
+            'Donation' => '0',
+            'CarrierType' => '1',
+            'TaxType' => '1',
+            'SalesAmount' => $saleAmount,
+            'Items' => [
+                [
+                    'ItemName' => '測試商品01',
+                    'ItemCount' => $itemCount,
+                    'ItemWord' => '個',
+                    'ItemPrice' => $itemPriceIncludeTax,
+                    'ItemTaxType' => '1',
+                    'ItemAmount' => $itemAmount,
+                ],
+            ],
+            'InvType' => '07'
+        ];
+        $input = [
+            'MerchantID' => '2000132',
+            'RqHeader' => [
+                'Timestamp' => time(),
+                'Revision' => '3.0.0',
+            ],
+            'Data' => $data,
+        ];
+        $url = 'https://einvoice-stage.ecpay.com.tw/B2CInvoice/Issue';
+
+        $response = $postService->post($input, $url);
 
         $response = $postService->post($input, $url);
 
         return $response;
-
     }
-
 }
