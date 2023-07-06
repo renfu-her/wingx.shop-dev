ec8dff36b24ac2143a2c035d08075bbf319ae0f7
Patrick Her <renfu.her@gmail.com>
2023-07-06 12:17:17 +0800

    update task

---
+++ app/Console/Commands/InvoiceCheck.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+use App\Models\Order;
+use App\Services\EcPayService;
+
+class InvoiceCheck extends Command
+{
+
+    protected $signature = 'InvoiceCheck';
+    protected $description = '檢查發票是否開立成功';
+
+    public function handle()
+    {
+
+        $orders = Order::where('status', 1)->where('invoice_no', '')->get();
+        Log::info('=== 發票 Start ==');
+        foreach($orders as $key => $value){
+            $ecpayService = new EcPayService();
+            $eInvoice = $ecpayService->ecpayInvoice($value->order_no);
+
+            if($eInvoice['TransCode'] == 1){
+
+                $e_invoice = $eInvoice['Data'];
+                $invoiceNo = $e_invoice['InvoiceNumber'] ?? $e_invoice['InvoiceNo'];
+                $order = Order::where('order_no', $value->order_no)->first();
+                $order->invoice_no = $invoiceNo
+                $order->invoice_random_no = $e_invoice['RandomNumber'];
+                $order->save();
+
+                Log::info('=== 發票成功 : ' . $invoiceNo . ' ==');
+            };
+        }
+        Log::info('=== 發票 End ==');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('OrderStatus')->everyTenMinutes();
+        $schedule->command('InvoiceCheck')->everyTenMinutes();
     }
 
     /**
