5d99276f3761ea71082aafea1d83db8ced91eb76
Patrick Her <renfu.her@gmail.com>
2023-07-13 23:01:52 +0800

    update

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -1,6 +1,17 @@
 @extends('frontend.layouts.master')
 
 @section('content')
+    <!-- Category Top Banner -->
+    <div class="py-6 bg-img-cover bg-dark bg-overlay-gradient-dark position-relative overflow-hidden mb-4 bg-pos-center-center"
+        style="background-image: url(./assets/images/banners/banner-1.jpg);">
+        <div class="container position-relative z-index-20" data-aos="fade-right" data-aos-delay="300">
+            <div class="col-12 col-md-6">
+                <p class="lead text-white mb-0">
+
+                </p>
+            </div>
+        </div>
+    </div>
     <!-- Main Section-->
     <section class="mt-5 container ">
         <!-- Page Content Goes Here -->
