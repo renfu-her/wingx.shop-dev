5a33f4294d96fbf1b121bb5f92acc738bea26c9b
Patrick Her <renfu.her@gmail.com>
2023-08-09 11:32:19 +0800

    update & compose require erusev/parsedown

---
+++ app/Http/Controllers/PolicyController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Parsedown;
+
+use App\Models\Policy;
+
+class PolicyController extends Controller
+{
+
+    // 隱私權政策
+    public function privacy_policy()
+    {
+
+        $data = Policy::find(1);
+
+        $parser = new Parsedown();
+        $content_markdown = $parser->text($data->content);
+
+        return view(
+            'frontend.privacy',
+            compact('data', 'content_markdown')
+        );
+    }
+}
