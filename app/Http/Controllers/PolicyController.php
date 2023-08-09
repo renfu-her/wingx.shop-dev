754716eb6e5b894e3cc11f600eec6b56c6d9e015
Patrick Her <renfu.her@gmail.com>
2023-08-09 20:25:16 +0800

    update

--- app/Http/Controllers/PolicyController.php
+++ app/Http/Controllers/PolicyController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use Parsedown;
 
 use App\Models\Policy;
+use App\Models\Banner;
+use App\Models\ProductImage;
+
+use App\Services\CartService;
 
 class PolicyController extends Controller
 {
@@ -18,6 +22,25 @@ class PolicyController extends Controller
 
         $parser = new Parsedown();
         $content_markdown = $parser->text($data->content);
+
+        $product_images = ProductImage::all();
+
+        $products = $this->getProduct();
+        $product_categories = $this->getProductCategory();
+
+        $banners = Banner::orderByDesc('id')->get();
+
+        $total = 0;
+        $tax = 0;
+        $cart = (new CartService())->getCartAll();
+        foreach($cart as $key => $value){
+            $total += $value['prod_price'] * $value['qty'];
+            $tax +=  ($value['prod_price'] * $value['qty']) * 0.05;
+        }
+
+        $cart_count = (new CartService())->getCart();
+        $cart_count = json_decode($cart_count->getContent(), true);
+
 
         return view(
             'privacy',
