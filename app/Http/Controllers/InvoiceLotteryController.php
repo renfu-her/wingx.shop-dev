3260665507922ab8a13fdb26002b3866a01ec5e7
Patrick Her <renfu.her@gmail.com>
2023-08-16 09:58:33 +0800

    update xnl to json than transfer DB

--- app/Http/Controllers/InvoiceLotteryController.php
+++ app/Http/Controllers/InvoiceLotteryController.php
@@ -11,9 +11,39 @@ class InvoiceLotteryController extends Controller
     // read xml convert json
     public function readXmlToJson()
     {
-        $xml = simplexml_load_file('https://invoice.etax.nat.gov.tw/invoice.xml');
-        $json = json_encode($xml);
-        $array = json_decode($json, true);
-        return $array;
+        $url = "https://invoice.etax.nat.gov.tw/invoice.xml";
+        $xmlString = file_get_contents($url);
+
+        if ($xmlString) {
+            $xml = simplexml_load_string($xmlString);
+
+            // 遍歷每個 item
+            foreach ($xml->channel->item as $item) {
+                $title = (string) $item->title;
+                $description = (string) $item->description;  // strip_tags() 可以移除 HTML 標籤
+                $description_strip_p = str_replace(['<p>', '</p>'], ['', '|'], $description);
+                $description_explode = explode('|', $description_strip_p);
+
+                $year_month = explode(" ", $title);
+                $year = $year_month[0];
+                $month = $year_month[1];
+                $special_bonus_arr = explode("：", $description_explode[0]);
+                $special_award_arr = explode("：", $description_explode[1]);
+                $jackpot_arr = explode("：", $description_explode[2]);
+
+                $invoiceLottery = InvoiceLottery::where('year', $year)->where('month', $month)->first();
+                if (!$invoiceLottery) {
+                    $invoiceLottery = new InvoiceLottery();
+                    $invoiceLottery->year = $year;
+                    $invoiceLottery->month = $month;
+                    $invoiceLottery->special_bonus = $special_bonus_arr[1];
+                    $invoiceLottery->special_award = $special_award_arr[1];
+                    $invoiceLottery->jackpot = $jackpot_arr[1];
+                    $invoiceLottery->save();
+                }
+            }
+        } else {
+            echo "Failed to load XML.";
+        }
     }
 }
