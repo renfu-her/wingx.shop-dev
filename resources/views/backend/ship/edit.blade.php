4c0a330e1c39d4545d83aab841f60035f37e40e5
Patrick Her <renfu.her@gmail.com>
2023-08-25 15:51:36 +0800

    update ship edit

--- resources/views/backend/ship/edit.blade.php
+++ resources/views/backend/ship/edit.blade.php
@@ -22,7 +22,7 @@
                             <div class="sbp-preview">
                                 <div class="sbp-preview-content">
                                     <x:form::form method="PUT" id="form_post" enctype="multipart/form-data"
-                                        :action="route('banner.update', $ships->id)" :bind="$ships">
+                                        :action="route('ship.update', $ships->id)" :bind="$ships">
 
                                         <div class="mt-3">
                                             <x:form::input name="name" label="標題" />
