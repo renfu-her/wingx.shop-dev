aac0766174ca981b3709b1165780dbd92af67628
Patrick Her <renfu.her@gmail.com>
2023-09-19 17:32:43 +0800

    update ships

--- resources/views/backend/product/edit.blade.php
+++ resources/views/backend/product/edit.blade.php
@@ -22,11 +22,11 @@
                             <div class="sbp-preview">
                                 <div class="sbp-preview-content">
                                     <x:form::form method="PUT" id="form_post" enctype="multipart/form-data"
-                                        :action="route('product.update', $product - > id)" :bind="$product">
+                                        :action="route('product.update', $product->id)" :bind="$product">
 
                                         <div class="mt-3">
                                             <x:form::select name="category_id" class="form-control" label="分類"
-                                                :options="$product_category" :selected="$product - > category_id" required />
+                                                :options="$product_category" :selected="$product->category_id" required />
                                         </div>
 
                                         <div class="mt-3">
