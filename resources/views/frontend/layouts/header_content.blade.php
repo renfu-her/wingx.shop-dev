93a0fd624b75b8f35f584e30367afadc7fe49d26
Patrick Her <renfu.her@gmail.com>
2023-09-21 10:21:59 +0800

    update category

--- resources/views/frontend/layouts/header_content.blade.php
+++ resources/views/frontend/layouts/header_content.blade.php
@@ -1,8 +1,7 @@
 <div class="w-100 pb-lg-0 pt-lg-0 pt-4 pb-3">
     <div class="container-fluid d-flex justify-content-between align-items-center flex-wrap">
 
-        <div class="d-flex align-items-center me-2 lh-1 width-percent" style="cursor: pointer"
-            onclick="location.href='/'">
+        <div class="d-flex align-items-center me-2 lh-1 width-percent" style="cursor: pointer" onclick="location.href='/'">
             <img src="{{ asset('frontend/images/logos/logo_c.svg') }}" style="width: 100% " alt="">
         </div>
 
@@ -25,6 +24,8 @@
                         全系列商品
                     </a>
                     <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/category/0">所有商品</a>
+                        </li>
                         @foreach ($product_categories as $product_category)
                             <li><a class="dropdown-item"
                                     href="/category/{{ $product_category->id }}">{{ $product_category->name }}</a>
@@ -52,10 +53,9 @@
 
             <!-- Navbar Toggle Icon-->
             <li class="d-inline-block d-lg-none">
-                <button
-                    class="btn btn-link px-2 text-decoration-none navbar-toggler border-0 d-flex align-items-center"
-                    data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="btn btn-link px-2 text-decoration-none navbar-toggler border-0 d-flex align-items-center"
+                    data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <i class="ri-menu-line ri-lg align-middle"></i>
                 </button>
             </li>
@@ -70,8 +70,7 @@
             @if (Session::has('member_id'))
                 <!-- 使用者登登出 -->
                 <li class="ms-1 d-lg-inline-block">
-                    <a class="btn btn-link px-2 py-0 text-decoration-none d-flex align-items-center"
-                        href="/logout">
+                    <a class="btn btn-link px-2 py-0 text-decoration-none d-flex align-items-center" href="/logout">
                         <i class="fa-solid fa-arrow-right-from-bracket"></i> 登出
                     </a>
                 </li>
@@ -80,8 +79,7 @@
             @if (Session::has('member_id'))
                 <!-- 直接導向購物車列表以頁面 -->
                 <li class="ms-1 d-lg-inline-block">
-                    <a class="btn btn-link px-2 text-decoration-none d-flex align-items-center"
-                        href="/order/list">
+                    <a class="btn btn-link px-2 text-decoration-none d-flex align-items-center" href="/order/list">
                         <i class="ri-user-line ri-lg align-middle"></i>
                     </a>
                 </li>
@@ -98,8 +96,7 @@
 
             <!-- 購物車-->
             <li class="ms-1 d-inline-block position-relative">
-                <button
-                    class="btn btn-link px-2 text-decoration-none d-flex align-items-center disable-child-pointer"
+                <button class="btn btn-link px-2 text-decoration-none d-flex align-items-center disable-child-pointer"
                     data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
                     <i class="ri-shopping-cart-2-line ri-lg align-middle position-relative z-index-10"></i> 購物車
                     @if ($cart_count['cart_count'] > 0)
