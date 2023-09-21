ec33406a58a7f820142c6a7b8f7cd9934b79fb0a
Patrick Her <renfu.her@gmail.com>
2023-09-21 10:29:17 +0800

    update category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,48 +13,59 @@ use App\Services\CartService;
 class CategoryController extends Controller
 {
 
-        // 分類頁
-        public function index(Request $request, $category_id = 0)
-        {
+    // 分類頁
+    public function index(Request $request, $category_id = 0)
+    {
 
-            $req = $request->all();
+        $req = $request->all();
 
-            // category_id = 0，代表全部
-            $category_id = $category_id == 0 ? 0 : $category_id;
-            if($category_id == 0) {
-                $products = Product::paginate(24);
-            } else{
-                $products = Product::where('category_id', $category_id)->paginate(24);
-            }
-
-            foreach ($products as $product) {
-                if($product->define_image == 0){
-                    $product->image_url = 'https://down-tw.img.susercontent.com/file/' .$product->image;
-                } else {
-                    $product->image_url = asset('upload/images/' . $product->id . '/' . $product->image);
-                }
-            }
-
-            $product_categories = ProductCategory::all();
-            foreach($product_categories as $product_category){
-                $product_count = Product::where('category_id', $product_category->id)->count();
-                $product_category->count = $product_count;
-            }
-
-            $total = 0;
-            $tax = 0;
-            $cart = (new CartService())->getCartAll();
-            foreach($cart as $key => $value){
-                $total += $value['prod_price'] * $value['qty'];
-                $tax +=  ($value['prod_price'] * $value['qty']) * 0.05;
-            }
-
-
-            $cart_count = (new CartService())->getCart();
-            $cart_count = json_decode($cart_count->getContent(), true);
-
-            return view('frontend.category',
-                compact('products', 'product_categories',
-                        'cart_count', 'cart', 'total', 'tax'));
+        // category_id = 0，代表全部
+        $category_id = $category_id == 0 ? 0 : $category_id;
+        if ($category_id == 0) {
+            $products = Product::paginate(24);
+        } else {
+            $products = Product::where('category_id', $category_id)->paginate(24);
         }
+
+        foreach ($products as $product) {
+            if ($product->define_image == 0) {
+                $product->image_url = 'https://down-tw.img.susercontent.com/file/' . $product->image;
+            } else {
+                $product->image_url = asset('upload/images/' . $product->id . '/' . $product->image);
+            }
+        }
+
+        $product_categories = ProductCategory::all();
+        foreach ($product_categories as $product_category) {
+            $product_count = Product::where('category_id', $product_category->id)->count();
+            $product_category->count = $product_count;
+        }
+
+        $total = 0;
+        $tax = 0;
+        $cart = (new CartService())->getCartAll();
+        foreach ($cart as $key => $value) {
+            $total += $value['prod_price'] * $value['qty'];
+            $tax +=  ($value['prod_price'] * $value['qty']) * 0.05;
+        }
+
+
+        $cart_count = (new CartService())->getCart();
+        $cart_count = json_decode($cart_count->getContent(), true);
+
+        $categoryCountAll = Product::count();
+
+        return view(
+            'frontend.category',
+            compact(
+                'products',
+                'product_categories',
+                'cart_count',
+                'cart',
+                'total',
+                'tax',
+                'categoryCountAll'
+            )
+        );
+    }
 }
--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -31,16 +31,27 @@
                                 <h2 class="mb-4 fs-6 mt-2 fw-bolder">商品目録</h2>
                                 <nav>
                                     <ul class="list-unstyled list-default-text">
-                                        @foreach($product_categories as $product_category)
-                                        <li class="mb-2"><a
-                                                class="text-decoration-none text-body text-secondary-hover transition-all d-flex justify-content-between align-items-center"
-                                                href="/category/{{ $product_category->id }}">
+                                        <li class="mb-2">
+                                            <a class="text-decoration-none text-body text-secondary-hover transition-all d-flex justify-content-between align-items-center"
+                                                href="/catehory/0">
                                                 <span>
                                                     <i class="ri-arrow-right-s-line align-bottom ms-n1"></i>
-                                                    {{ $product_category->name }}
+                                                    所有商品
                                                 </span>
-                                                <span class="text-muted ms-4">({{ $product_category->count }})</span></a>
+                                                <span class="text-muted ms-4">{{ $categoryCountAll }}</span>
+                                            </a>
                                         </li>
+                                        @foreach ($product_categories as $product_category)
+                                            <li class="mb-2"><a
+                                                    class="text-decoration-none text-body text-secondary-hover transition-all d-flex justify-content-between align-items-center"
+                                                    href="/category/{{ $product_category->id }}">
+                                                    <span>
+                                                        <i class="ri-arrow-right-s-line align-bottom ms-n1"></i>
+                                                        {{ $product_category->name }}
+                                                    </span>
+                                                    <span
+                                                        class="text-muted ms-4">({{ $product_category->count }})</span></a>
+                                            </li>
                                         @endforeach
                                     </ul>
                                 </nav>
@@ -57,33 +68,33 @@
                     <div class="row g-4 mb-5">
 
                         @forelse($products as $product)
-                        <div class="col-6 col-sm-6 col-md-4">
-                            <!-- Card Product-->
-                            <div class="card position-relative h-100 card-listing hover-trigger">
-                                <div class="card-header">
-                                    <picture class="position-relative overflow-hidden d-block bg-light">
-                                        <img class="w-100 img-fluid position-relative z-index-10" title=""
-                                            src="{{ $product->image_url }}" alt="">
-                                    </picture>
-                                    <picture class="position-absolute z-index-20 start-0 top-0 hover-show bg-light">
-                                        <img class="w-100 img-fluid" title=""
-                                            src="{{ $product->image_url }}" alt="">
-                                    </picture>
+                            <div class="col-6 col-sm-6 col-md-4">
+                                <!-- Card Product-->
+                                <div class="card position-relative h-100 card-listing hover-trigger">
+                                    <div class="card-header">
+                                        <picture class="position-relative overflow-hidden d-block bg-light">
+                                            <img class="w-100 img-fluid position-relative z-index-10" title=""
+                                                src="{{ $product->image_url }}" alt="">
+                                        </picture>
+                                        <picture class="position-absolute z-index-20 start-0 top-0 hover-show bg-light">
+                                            <img class="w-100 img-fluid" title="" src="{{ $product->image_url }}"
+                                                alt="">
+                                        </picture>
+                                    </div>
+                                    <div class="card-body px-0 text-center">
+                                        <a class="mb-0 mx-2 mx-md-4 fs-p link-cover text-decoration-none d-block text-center"
+                                            href="/product/{{ $product->id }}">{{ $product->name }}</a>
+                                        <p class="fw-bolder m-0 mt-2">$ {{ number_format($product->price) }}</p>
+                                    </div>
                                 </div>
-                                <div class="card-body px-0 text-center">
-                                    <a class="mb-0 mx-2 mx-md-4 fs-p link-cover text-decoration-none d-block text-center"
-                                        href="/product/{{ $product->id }}">{{ $product->name }}</a>
-                                    <p class="fw-bolder m-0 mt-2">$ {{ number_format($product->price) }}</p>
-                                </div>
+                                <!--/ Card Product-->
                             </div>
-                            <!--/ Card Product-->
-                        </div>
                         @empty
-                        <div class="col-12 col-sm-6 col-md-12 d-flex justify-content-center">
-                            <div class="p-5" style="font-size: 26px">
-                                尚未有商品
+                            <div class="col-12 col-sm-6 col-md-12 d-flex justify-content-center">
+                                <div class="p-5" style="font-size: 26px">
+                                    尚未有商品
+                                </div>
                             </div>
-                        </div>
                         @endforelse
                     </div>
                     <!-- / Products-->
@@ -105,12 +116,12 @@
 
 @section('css')
     <style>
-        img, svg {
+        img,
+        svg {
             width: 16px;
         }
     </style>
 @endsection
 
 @section('js')
-
 @endsection
