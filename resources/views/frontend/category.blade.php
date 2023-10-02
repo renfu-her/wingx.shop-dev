101194b2754bd7824f75a8204a0424ba51ffa370
Patrick Her <renfu.her@gmail.com>
2023-10-02 10:52:57 +0800

    update 名稱為 category

--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -33,7 +33,7 @@
                                     <ul class="list-unstyled list-default-text">
                                         <li class="mb-2">
                                             <a class="text-decoration-none text-body text-secondary-hover transition-all d-flex justify-content-between align-items-center"
-                                                href="/catehory/0">
+                                                href="/category/0">
                                                 <span>
                                                     <i class="ri-arrow-right-s-line align-bottom ms-n1"></i>
                                                     所有商品
