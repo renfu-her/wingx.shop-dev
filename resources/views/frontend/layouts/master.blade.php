f830e0d19d411198760bcbad0c9aabc4365bed46
Patrick Her <renfu.her@gmail.com>
2024-04-10 15:10:15 +0800

    update

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -232,7 +232,7 @@
                                     <div>
                                         <h6 class="justify-content-between d-flex align-items-start mb-2">
                                             {{ $value['prod_name'] }}
-                                            <i class="ri-close-line"></i>
+                                            <i class="ri-close-line" onclick="cartDelete({{ $value['id'] }})"></i>
                                         </h6>
                                         <small class="d-block fw-bolder">{!! $value['items'] !!}</small>
                                         <small class="d-block text-muted fw-bolder">數量: {{ $value['qty'] }}</small>
@@ -265,6 +265,18 @@
     @include('frontend.layouts.footer_include')
 
     @yield('js')
+
+    <script>
+        const cartDelete = (deleteId) => {
+            if (confirm('確定要刪除？')) {
+                $.post('/cart/delete', {
+                    'deleteId': deleteId,
+                }, function() {
+                    location.reload();
+                })
+            }
+        }
+    </script>
 </body>
 
 </html>
