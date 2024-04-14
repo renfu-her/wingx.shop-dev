a6ac0896dc6822783003521366e1238500042fb7
Patrick Her <renfu.her@gmail.com>
2024-04-14 17:17:54 +0800

    update

--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -427,29 +427,29 @@
         }
 
         //MARK: returnedData, json to array
-        // window.addEventListener('storage', function(event) {
-        //     if (event.key === 'returnedData') {
-        //         console.log('Key changed:', event.key);
-        //         // console.log('New value:', event.newValue);
-        //         // 從 JSON 字符串解析回陣列
-        //         let returnedArray = JSON.parse(event.newValue);
+        window.addEventListener('storage', function(event) {
+            if (event.key === 'returnedData') {
+                console.log('Key changed:', event.key);
+                // console.log('New value:', event.newValue);
+                // 從 JSON 字符串解析回陣列
+                let returnedArray = JSON.parse(event.newValue);
 
-        //         $('input[name=LogisticsSubType]').val(returnedArray['LogisticsSubType']);
-        //         $('input[name=CVSStoreID]').val(returnedArray['CVSStoreID']);
-        //         $('input[name=CVSStoreName]').val(returnedArray['CVSStoreName']);
-        //         $('input[name=CVSAddress]').val(returnedArray['CVSAddress']);
-        //         $('input[name=CVSTelephone]').val(returnedArray['CVSTelephone']);
-        //         $('input[name=CVSOutSide]').val(returnedArray['CVSOutSide']);
+                $('input[name=LogisticsSubType]').val(returnedArray['LogisticsSubType']);
+                $('input[name=CVSStoreID]').val(returnedArray['CVSStoreID']);
+                $('input[name=CVSStoreName]').val(returnedArray['CVSStoreName']);
+                $('input[name=CVSAddress]').val(returnedArray['CVSAddress']);
+                $('input[name=CVSTelephone]').val(returnedArray['CVSTelephone']);
+                $('input[name=CVSOutSide]').val(returnedArray['CVSOutSide']);
 
-        //         $('#storeDisplay').show();
-        //         $('#CVSStoreID').html('代號：' + returnedArray['CVSStoreID'])
-        //         $('#CVSStoreName').html('名稱：' + returnedArray['CVSStoreName'])
-        //         $('#CVSAddress').html('地址：' + returnedArray['CVSAddress'])
-        //         $('#CVSTelephone').html('電話：' + returnedArray['CVSTelephone'])
+                $('#storeDisplay').show();
+                $('#CVSStoreID').html('代號：' + returnedArray['CVSStoreID'])
+                $('#CVSStoreName').html('名稱：' + returnedArray['CVSStoreName'])
+                $('#CVSAddress').html('地址：' + returnedArray['CVSAddress'])
+                $('#CVSTelephone').html('電話：' + returnedArray['CVSTelephone'])
 
-        //         // 清除數據或進行其他處理
-        //         localStorage.removeItem('returnedData');
-        //     }
-        // });
+                // 清除數據或進行其他處理
+                localStorage.removeItem('returnedData');
+            }
+        });
     </script>
 @endsection
