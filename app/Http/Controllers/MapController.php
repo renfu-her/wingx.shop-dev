39762e4e2ba6c5fb0552ec2ba2b55de065e6d7a6
Patrick Her <renfu.her@gmail.com>
2024-04-16 11:27:22 +0800

    update

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -10,20 +10,17 @@ use Illuminate\Support\Facades\Http;
 class MapController extends Controller
 {
     // rewrite
-    public function rewrite(Request $request, $sessionId)
+    public function rewrite(Request $request)
     {
         $dataArray = $request->all();
 
         $data = [
             'message' => $dataArray
         ];
-        
+
         // 轉換為 JSON 字符串
         $dataArrayJson = json_encode($data);
 
-        // session id
-        // session()->setId($sessionId);
-        // dd($sessionId, $dataArrayJson);
 
         return view('frontend.mapReturn', compact('dataArrayJson'));
     }
