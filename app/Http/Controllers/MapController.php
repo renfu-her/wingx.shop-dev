212a7c1d35b5b3a79445997236899bf8cbd39ccb
Patrick Her <renfu.her@gmail.com>
2024-04-16 09:02:05 +0800

    update

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -9,25 +9,7 @@ use Illuminate\Support\Facades\Http;
 
 class MapController extends Controller
 {
-    // index
-    public function index(Request $request)
-    {
-
-        // dd(env('EXPRESS_MAP_URL'), env('EXPRESS_MERCHANT_ID'));
-        $response = Http::asForm()
-            ->withHeaders([
-                'Accept' => 'text/html',
-                'Content-Type' => 'application/x-www-form-urlencoded',
-            ])
-            ->post('https://logistics-stage.ecpay.com.tw/Express/map', [
-                'MerchantID' => '2000933',
-                'LogisticsType' => 'CVS',
-                'LogisticsSubType' => 'UNIMARTC2C',
-                'IsCollection' => 'N',
-                'ServerReplyURL' => 'https://wingx-shop.dev-laravel.co/cart/rewrite',
-            ]);
-    }
-
+    // rewrite
     public function rewrite(Request $request, $sessionId)
     {
         $dataArray = $request->all();
