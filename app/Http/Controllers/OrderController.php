ddae505d5488a1c7183d72fa35067b26ae0030ef
Patrick Her <renfu.her@gmail.com>
2024-04-24 14:20:43 +0800

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -312,11 +312,11 @@ class OrderController extends Controller
         // 4) 最前方加入 HashKey，最後方加入 HashIV
         $paramsString = "HashKey={$hashKey}&{$paramsString}&HashIV={$hashIV}";
 
-        dd($paramsString);
+        // dd($paramsString);
 
         // 5) 做 URLEncode
         $paramsString = urlencode($paramsString);
-
+        
         // 6) 轉為全小寫
         $paramsString = strtolower($paramsString);
 
