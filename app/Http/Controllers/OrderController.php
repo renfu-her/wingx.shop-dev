6bdbc21f1601864728c370a99af26ef6b70b457c
Patrick Her <renfu.her@gmail.com>
2024-04-24 15:16:44 +0800

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -172,7 +172,7 @@ class OrderController extends Controller
                 'CollectionAmount' => $ttl_total,
                 // 'IsCollection' => 'N',
                 // 'GoodsName' => $desc,
-                'SenderName' => '翼優寵物賣家',
+                'SenderName' => '翼優寵物',
                 'SenderPhone' => $req['mobile'],
                 // 'SenderCellPhone' => $req['mobile'],
                 'ReceiverName' => $req['name'],
