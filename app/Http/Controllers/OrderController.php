7bee07d7d349c4de134071e081dfa922ac363f13
Patrick Her <renfu.her@gmail.com>
2024-04-24 17:11:49 +0800

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -187,7 +187,7 @@ class OrderController extends Controller
 
             $orderData = Http::asForm()->post($mapUrl, $logisticsData);
 
-            // return view('frontend.order.ecpayStore', compact('orderData'));
+            return view('frontend.order.store', compact('orderData'));
         } else {
 
             $formData = [
