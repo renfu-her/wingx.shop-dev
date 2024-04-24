c98069c175321d7f20659d1be63ffdc979f63fa9
Patrick Her <renfu.her@gmail.com>
2024-04-24 09:26:29 +0800

    update config.php

--- config/config.php
+++ config/config.php
@@ -20,15 +20,9 @@ return [
 
     "EXPRESS_URL" => env("EXPRESS_URL", "https://logistics-stage.ecpay.com.tw/Express/v2/"),
     "EXPRESS_MAP_URL" => env("EXPRESS_MAP_URL", "https://logistics-stage.ecpay.com.tw/Express/map"),
-<<<<<<< HEAD
-    "EXPRESS_MERCHANT_ID" => env("EXPRESS_MERCHANT_ID", "2000933"),
-    "EXPRESS_HASH_KEY" => env("EXPRESS_HASH_KEY", "XBERn1YOvpM9nfZc"),
-    "EXPRESS_HASH_IV" => env("EXPRESS_HASH_IV", "h1ONHk4P4yqbl5LK"),
-=======
     "EXPRESS_MERCHANT_ID" => env("EXPRESS_MERCHANT_ID", "3383763"),
     "EXPRESS_HASH_KEY" => env("EXPRESS_HASH_KEY", "jIS2talQzOhj9Hmp"),
     "EXPRESS_HASH_IV" => env("EXPRESS_HASH_IV", "HJz5BtcreByPP2Z5"),
->>>>>>> 6e1c75617e8ca66d2891eef00b62d42e07736da7
     "EXPRESS_VISION" => env("EXPRESS_VISION", "1.0.0"),
     "EXPRESS_MERCHANT_ID_DEV" => env("EXPRESS_MERCHANT_ID_DEV", "2000933"),
     "EXPRESS_HASH_KEY_DEV" => env("EXPRESS_HASH_KEY_DEV", "XBERn1YOvpM9nfZc"),
