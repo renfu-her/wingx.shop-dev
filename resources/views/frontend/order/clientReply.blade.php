b3f2eb815f586385ca5e0bb7b679a2a569c94f51
Patrick Her <renfu.her@gmail.com>
2024-04-24 17:50:40 +0800

    update

--- resources/views/frontend/order/clientReply.blade.php
+++ resources/views/frontend/order/clientReply.blade.php
@@ -7,28 +7,31 @@
                 <div class="card border-0 shadow rounded-3 my-5">
                     <div class="card-body p-4 p-sm-5">
                         <h5 class="card-title text-center mb-5 fw-light fs-5">店到店</h5>
-                        <div class="col-12 col-md-6">
+                        <div class="col-12 col-md-12">
                             <div class="form-group">
                                 <label for="type" class="form-label">物流狀態 </label>
                                 <input type="text" class="form-control-plaintext" value="{{ $data['RtnMsg'] }}" readonly>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <div class="col-12 col-md-12">
                             <div class="form-group">
                                 <label for="type" class="form-label">物流類型 </label>
-                                <input type="text" class="form-control-plaintext" value="{{ $logisticsSubType }}" readonly>
+                                <input type="text" class="form-control-plaintext" value="{{ $logisticsSubType }}"
+                                    readonly>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <div class="col-12 col-md-12">
                             <div class="form-group">
                                 <label for="type" class="form-label">寄貨編號 </label>
-                                <input type="text" class="form-control-plaintext" value="{{ $data['CVSPaymentNo'] }}" readonly>
+                                <input type="text" class="form-control-plaintext" value="{{ $data['CVSPaymentNo'] }}"
+                                    readonly>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <div class="col-12 col-md-12">
                             <div class="form-group">
                                 <label for="type" class="form-label">物流交易編號 </label>
-                                <input type="text" class="form-control-plaintext" value="{{ $data['AllPayLogisticsID'] }}" readonly>
+                                <input type="text" class="form-control-plaintext"
+                                    value="{{ $data['AllPayLogisticsID'] }}" readonly>
                             </div>
                         </div>
                     </div>
@@ -36,4 +39,13 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('css')
+    <style>
+        .form-label {
+            font-size: 18px !important;
+            font-weight: 600 !important;
+        }
+    </style>
 @endsection
