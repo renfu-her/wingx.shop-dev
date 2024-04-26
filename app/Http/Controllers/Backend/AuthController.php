b0c4a50e69e98e6aa6d7e2ae13425109c0de868d
Patrick Her <renfu.her@gmail.com>
2024-04-26 15:22:27 +0800

    Refactor login_verify method in AuthController.php and add Auth::check() for debugging

--- app/Http/Controllers/Backend/AuthController.php
+++ app/Http/Controllers/Backend/AuthController.php
@@ -21,6 +21,7 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
+        
         if (Auth::attempt($credentials)) {
             dd(Auth::check());
             return redirect('/backend/product');
