087bf1cc5f69b835544bece8816832c739a81458
Patrick Her <renfu.her@gmail.com>
2024-05-04 16:51:32 +0800

    Refactor color-coded status indicators in orderList.blade.php

--- resources/views/frontend/order/orderList.blade.php
+++ resources/views/frontend/order/orderList.blade.php
@@ -83,7 +83,7 @@
                                         <div class="col-12 col-md-3" style="font-weight: 600">
                                             @if (!empty($value->cvs_store_id))
                                                 @if ($value->logistics_status == 2030 || $value->logistics_status == 3024)
-                                                    <span class="text-icon" style="color: lightgreen">商品已送至物流中心</span>
+                                                    <span class="text-icon" style="color: darkgreen">商品已送至物流中心</span>
                                                 @elseif($value->logistics_status == 2063 || $value->logistics_status == 2073 || $value->logistics_status == 3018)
                                                     <span class="text-icon" style="color: green">商品已送達門市</span>
                                                 @elseif($value->logistics_status == 2067 || $value->logistics_status == 3022)
@@ -93,7 +93,7 @@
                                                 @endif
                                             @else
                                                 @if ($value->status == 0 || $value->status == 9)
-                                                    <span class="text-icon" style="color: lightgreen">待付款</span>
+                                                    <span class="text-icon" style="color: darkgreen">待付款</span>
                                                 @elseif($value->status == 1)
                                                     <span class="text-icon" style="color: blue">完成付款</span>
                                                 @elseif($value->status == 2)
