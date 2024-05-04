7cb4b584592a04aec2ca780e8c330424a6cc936a
Patrick Her <renfu.her@gmail.com>
2024-05-04 12:44:50 +0800

    Add migration to add logistics_status column to orders table

---
+++ database/migrations/2024_05_04_124338_add_logistics_status_to_orders_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('logistics_status')->default(0)->comment('物流狀態');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
