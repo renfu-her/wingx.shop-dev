0b0cffe9f3dc431e4e31499d2154f46f10e21d1d
Patrick Her <renfu.her@gmail.com>
2024-06-11 14:58:43 +0800

    refactor: Add Cros middleware to handle CORS headers in incoming requests

---
+++ app/Http/Middleware/Cros.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cros
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+}
